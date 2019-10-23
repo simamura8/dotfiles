d207baba77e9e37aa8292f1354e78d9a11cc71be
s.imamura <s.imamura@edge-consulting.jp>
2019-10-23 13:27:48 +0900

    改良

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-cd ~/.vim/bundle/youcompleteme
-python3 install.py --all
-
-/bin/sh dotfilesLink.sh
-
-:source %
-:PluginInstall
+. link.sh
--- dotfilesLink.sh
+++
@@ -1,7 +0,0 @@
-#ln -s -i .vimrc /home/simamura/.vimrc
-#ln -s -i .gvimrc /home/simamura/.gvimrc
-#ln -s -i .bashrc /home/simamura/.bashrc
-
-ln -s -i ~/dotfiles/dotfiles/.vimrc ~/.vimrc
-ln -s -i ~/dotfiles/dotfiles/.gvimrc ~/.gvimrc
-ln -s -i ~/dotfiles/dotfiles/.bashrc ~/.bashrc
